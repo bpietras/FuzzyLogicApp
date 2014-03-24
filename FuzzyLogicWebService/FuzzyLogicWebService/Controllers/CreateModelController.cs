16a480c4f7aa9c005f937b2e1316063fd11aa7e7
Barbara Pietras <barbarapietrass@gmail.com>
2014-03-24 23:04:24 +0100

    First test tab of creating model

---
+++ FuzzyLogicWebService/FuzzyLogicWebService/Controllers/CreateModelController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FuzzyLogicWebService.FISFiles.FISModel;
+
+namespace FuzzyLogicWebService.Controllers
+{
+    public class CreateModelController : Controller
+    {
+        //
+        // GET: /CreateModel/
+
+        public ActionResult CheckFirstTab(FISSystem system)
+        {
+            return View("FirstTabReview", system);
+        }
+
+    }
+}
--- FuzzyLogicWebService/FuzzyLogicWebService/FISFiles/FISModel/FISSystem.cs
+++ FuzzyLogicWebService/FuzzyLogicWebService/FISFiles/FISModel/FISSystem.cs
@@ -2,19 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace FuzzyLogicWebService.FISFiles.FISModel
 {
     public class FISSystem
     {
+        [Required(ErrorMessage="Podaj nazwę modelu")]
+        [Display(Name="Nazwa systemu")]
         public String Name { get; set; }
+        [Required(ErrorMessage="Podaj liczbe wejść")]
+        [Range(1,10,ErrorMessage="Liczba wejść musi mieścić się w przedziale 1-10")]
         public int InputsNumber { get; set; }
+        [Required(ErrorMessage = "Podaj liczbe wyjść")]
+        [Range(1, 10, ErrorMessage = "Liczba wyjść musi mieścić się w przedziale 1-10")]
         public int OutputsNumber { get; set; }
+        [Required(ErrorMessage="Podaj liczbę reguł")]
+        [Range(1, 100,ErrorMessage="Liczba reguł musi mieścić się w przedziale 1-100")]
         public int RulesNumber { get; set; }
+        [HiddenInput(DisplayValue=false)]
         public String AndMethod { get; set; }
+        [HiddenInput(DisplayValue = false)]
         public String OrMethod { get; set; }
+        [HiddenInput(DisplayValue = false)]
         public String ImpMethod { get; set; }
+        [HiddenInput(DisplayValue = false)]
         public String AggMethod { get; set; }
+        [HiddenInput(DisplayValue = false)]
         public String DefuzzMethod { get; set; }
     }
 }
