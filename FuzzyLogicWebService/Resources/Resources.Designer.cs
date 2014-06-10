2c8ef1b86874f340228a33c5aa415150d6b3d663
Barbara Pietras <barbara.pietras@rulefinancial.com>
2014-06-10 10:34:01 +0200

    Adding resources for PL

--- FuzzyLogicWebService/Resources/Resources.Designer.cs
+++ FuzzyLogicWebService/Resources/Resources.Designer.cs
@@ -61,6 +61,24 @@ namespace Resources {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Opis:.
+        /// </summary>
+        public static string Description {
+            get {
+                return ResourceManager.GetString("Description", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Podaj opis.
+        /// </summary>
+        public static string DescriptionErrorMsg {
+            get {
+                return ResourceManager.GetString("DescriptionErrorMsg", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Krótki opis.
         /// </summary>
         public static string DescriptionString {
@@ -70,11 +88,218 @@ namespace Resources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Dalej.
+        ///   Looks up a localized string similar to Pierwsza charakterystyczna wartość.
         /// </summary>
-        public static string Next {
+        public static string FirstValue {
             get {
-                return ResourceManager.GetString("Next", resourceCulture);
+                return ResourceManager.GetString("FirstValue", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Podaj pierwszą charakterystyczną wartość.
+        /// </summary>
+        public static string FirstValueErrorMsg {
+            get {
+                return ResourceManager.GetString("FirstValueErrorMsg", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Czwarta charakterystyczna wartość.
+        /// </summary>
+        public static string FourthValue {
+            get {
+                return ResourceManager.GetString("FourthValue", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Rodzaj:.
+        /// </summary>
+        public static string FunctionType {
+            get {
+                return ResourceManager.GetString("FunctionType", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Nazwa zmiennej:.
+        /// </summary>
+        public static string FVariableName {
+            get {
+                return ResourceManager.GetString("FVariableName", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Podaj nazwę zmiennej.
+        /// </summary>
+        public static string FVariableNameErrorMsg {
+            get {
+                return ResourceManager.GetString("FVariableNameErrorMsg", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Liczba wejść:.
+        /// </summary>
+        public static string InputsNumber {
+            get {
+                return ResourceManager.GetString("InputsNumber", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Podaj liczbe wejść.
+        /// </summary>
+        public static string InputsNumberErrorMsg {
+            get {
+                return ResourceManager.GetString("InputsNumberErrorMsg", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Maksymalna wartość zmiennej:.
+        /// </summary>
+        public static string MaxValue {
+            get {
+                return ResourceManager.GetString("MaxValue", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Podaj maksymalną wartość zmiennej.
+        /// </summary>
+        public static string MaxValueErrorMsg {
+            get {
+                return ResourceManager.GetString("MaxValueErrorMsg", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Nazwa.
+        /// </summary>
+        public static string MembershipFunctionName {
+            get {
+                return ResourceManager.GetString("MembershipFunctionName", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Podaj nazwę funkcji przynależności.
+        /// </summary>
+        public static string MembershipFunctionNameErrorMsg {
+            get {
+                return ResourceManager.GetString("MembershipFunctionNameErrorMsg", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Minimalna wartość zmiennej:.
+        /// </summary>
+        public static string MinValue {
+            get {
+                return ResourceManager.GetString("MinValue", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Podaj minimalną wartość zmiennej.
+        /// </summary>
+        public static string MinValueErrorMsg {
+            get {
+                return ResourceManager.GetString("MinValueErrorMsg", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Liczba funkcji przynależności:.
+        /// </summary>
+        public static string NumberOfMembFunc {
+            get {
+                return ResourceManager.GetString("NumberOfMembFunc", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Podaj liczbę funkcji przynależności.
+        /// </summary>
+        public static string NumberOfMembFuncErrorMsg {
+            get {
+                return ResourceManager.GetString("NumberOfMembFuncErrorMsg", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Liczba wyjść:.
+        /// </summary>
+        public static string OutputsNumber {
+            get {
+                return ResourceManager.GetString("OutputsNumber", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Podaj liczbe wyjść.
+        /// </summary>
+        public static string OutputsNumberErrorMsg {
+            get {
+                return ResourceManager.GetString("OutputsNumberErrorMsg", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Podaj regułę.
+        /// </summary>
+        public static string RuleContent {
+            get {
+                return ResourceManager.GetString("RuleContent", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Reguła jest wymagana.
+        /// </summary>
+        public static string RuleContentErrorMsg {
+            get {
+                return ResourceManager.GetString("RuleContentErrorMsg", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Liczba reguł:.
+        /// </summary>
+        public static string RulesNumber {
+            get {
+                return ResourceManager.GetString("RulesNumber", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Podaj liczbę reguł.
+        /// </summary>
+        public static string RulesNumberErrorMsg {
+            get {
+                return ResourceManager.GetString("RulesNumberErrorMsg", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Druga charakterystyczna wartość.
+        /// </summary>
+        public static string SecondValue {
+            get {
+                return ResourceManager.GetString("SecondValue", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Podaj drugą charakterystyczną wartość.
+        /// </summary>
+        public static string SecondValueErrorMsg {
+            get {
+                return ResourceManager.GetString("SecondValueErrorMsg", resourceCulture);
             }
         }
         
@@ -93,6 +318,69 @@ namespace Resources {
         public static string SystemNameErrorMsg {
             get {
                 return ResourceManager.GetString("SystemNameErrorMsg", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Trzecia charakterystyczna wartość.
+        /// </summary>
+        public static string ThirdValue {
+            get {
+                return ResourceManager.GetString("ThirdValue", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Podaj trzecią charakterystyczną wartość.
+        /// </summary>
+        public static string ThirdValueErrorMsg {
+            get {
+                return ResourceManager.GetString("ThirdValueErrorMsg", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Wybierz rodzaj funkcji przynależności.
+        /// </summary>
+        public static string TypeErrorMsg {
+            get {
+                return ResourceManager.GetString("TypeErrorMsg", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Nazwa użytkownika:.
+        /// </summary>
+        public static string UserName {
+            get {
+                return ResourceManager.GetString("UserName", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Podaj nazwę użytkownika.
+        /// </summary>
+        public static string UserNameErrorMsg {
+            get {
+                return ResourceManager.GetString("UserNameErrorMsg", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Hasło:.
+        /// </summary>
+        public static string UserPassword {
+            get {
+                return ResourceManager.GetString("UserPassword", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Podaj hasło.
+        /// </summary>
+        public static string UserPasswordErrorMsg {
+            get {
+                return ResourceManager.GetString("UserPasswordErrorMsg", resourceCulture);
             }
         }
     }
